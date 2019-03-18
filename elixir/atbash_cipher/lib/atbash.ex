934f3c175b8854a96cba909796fa33f8a6ce4d59
mark182182 <versalife11@hotmail.com>
2019-03-18 22:10:01 +0100

    feat(decode_to_atbash): decode the input word to atbash cipher

--- elixir/atbash_cipher/lib/atbash.ex
+++ elixir/atbash_cipher/lib/atbash.ex
@@ -1,17 +1,19 @@
 defmodule Atbash do
   def main do
-    decode_to_atbash()
+    List.to_string(decode_to_atbash())
   end
 
   def decode_to_atbash do
     default_alphabet = "abcdefghijklmnopqrstuvwxyz"
     atbash_alphabet = "zyxwvutsrqponmlkjihgfedcba"
-
-    Enum.each(String.to_charlist(get_input_from_user()), fn character -> IO.puts <<character>> end)
+    Enum.map(String.to_charlist(get_input_from_user()), fn character ->
+      character_index = :binary.match default_alphabet, <<character>>
+      character_index = Kernel.elem(character_index, 0)
+      String.at(atbash_alphabet, character_index)
+    end)
   end
-
   def get_input_from_user do
-    user_input = IO.gets("Please input a sentence to decode!\n")
-    String.trim(user_input)
+    user_input = IO.gets("Please input a word to decode!\n")
+    String.downcase(String.trim(user_input))
   end
 end
