a525de6b6df89f6d046759ade70ca93ef5a07def
mark182182 <versalife11@hotmail.com>
2021-06-07 22:20:31 +0200

    Added shader files

--- cpp/04/main.cpp
+++ cpp/04/main.cpp
@@ -1,6 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <string>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
@@ -13,8 +15,61 @@ void processInput(GLFWwindow *window)
     glfwSetWindowShouldClose(window, true);
 }
 
+std::string read_file(std::string filename)
+{
+  std::ifstream file;
+  std::string output;
+  file.open(filename);
+
+  if (file.good() && file.is_open())
+  {
+    while (file)
+    {
+      std::string buff;
+      std::getline(file, buff);
+      output += buff.append("\n");
+    }
+    file.close();
+    return output;
+  }
+  else
+  {
+    std::cerr << "Error reading file: " + filename << std::endl;
+    return NULL;
+  }
+}
+
+void check_shader_compile_status(unsigned int vertexShader)
+{
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+              << infoLog << std::endl;
+  }
+}
+
+void check_shader_program_compile_status(unsigned int shaderProgram)
+{
+  int success;
+  char infoLog[512];
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success)
+  {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n"
+              << infoLog << std::endl;
+  }
+}
+
 int main()
 {
+  std::string vertexShaderStr = read_file("shaders/vertex.vs");
+  std::string fragmentShaderStr = read_file("shaders/fragment.fs");
+
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -35,6 +90,55 @@ int main()
   }
   glViewport(0, 0, 800, 600);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // Shader ordering is important:
+  // 1. We create the vertex shader to assemble the shape
+  unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  const char *vtxShdrSource = vertexShaderStr.c_str();
+  glShaderSource(vertexShader, 1, &vtxShdrSource, NULL);
+  glCompileShader(vertexShader);
+
+  // 2. We create the fragment shader to colorize the shape
+  unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  const char *frgmntShdrSource = fragmentShaderStr.c_str();
+  glShaderSource(fragmentShader, 1, &frgmntShdrSource, NULL);
+  glCompileShader(fragmentShader);
+
+  check_shader_compile_status(vertexShader);
+  check_shader_compile_status(fragmentShader);
+
+  unsigned int shaderProgram = glCreateProgram();
+
+  // We attach the shaders in order to the program
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  check_shader_program_compile_status(shaderProgram);
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  float vertices[] = {
+      -0.5f, -0.5f, 0.0f,
+      0.5f, -0.5f, 0.0f,
+      0.0f, 0.5f, 0.0f};
+
+  unsigned int VBO, VAO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
+
   while (!glfwWindowShouldClose(window))
   {
     processInput(window);
@@ -42,9 +146,17 @@ int main()
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    // Swap front and back buffers
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteProgram(shaderProgram);
   glfwTerminate();
   return 0;
 }
