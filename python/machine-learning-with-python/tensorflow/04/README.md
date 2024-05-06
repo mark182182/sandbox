3c38eb3e01cb1f3984bddc976e38b87005f31de6
mark182182 <versalife11@hotmail.com>
2024-05-06 23:09:44 +0200

    Add tensorflow 04

---
+++ python/machine-learning-with-python/tensorflow/04/README.md
@@ -0,0 +1,16 @@
+## Core Learning Algorithms: Working with Data
+### Notes:
+- Focus on exporing, cleaning and selecting appropriate data
+- As an example we will use the Titanic dataset
+- Load the dataset [main.py](main.py)
+- ```pd.read_csv()`` will return to us a new pandas dataframe.
+  - A dataframe is like a table and we can actually look at the table representation
+- ```.head()``` function can be used to look at the data, it will show the first 5 items in our dataframe
+- After analyzing this information, we should notice the following:
+  - The manjority of passengers an in their 20's or 30's, are male and in the "Third" class
+  - Females have a much higher change of survival
+
+### Questions:
+What does the pandas .head() function do?
+
+- By default, shows the first five rows or entries in a data frame.
---
+++ python/machine-learning-with-python/tensorflow/04/main.py
@@ -0,0 +1,38 @@
+import numpy as np # very optimized way to handle arrays
+import pandas as pd # data analytics tool (storing, loading, visualizing data, stc.)
+
+import matplotlib.pyplot as plt
+from six.moves import urllib
+import tensorflow as tf
+
+def main():
+  dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv')
+  dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
+  y_train = dftrain.pop('survived') # we are dropping the survived column from dftrain
+  y_eval = dfeval.pop('survived')
+  
+  print(dftrain['age']) # show the values for the age column
+  print(dftrain.loc[0]) # the row values for the first column
+  print(dftrain.describe()) # gives an overall information on the data (mean, std, min etc.)
+  print(dftrain.shape) # the shape of the tensor
+  
+  plt.figure()  # Create a new figure
+  dftrain.age.hist(bins=20)  # Plot the histogram
+  plt.savefig('age_histogram.png')  # Save the figure to a PNG file
+  
+  plt.figure()
+  dftrain.sex.value_counts().plot(kind="barh")
+  plt.savefig('sex_distribution.png')
+  
+  plt.figure()
+  dftrain['class'].value_counts().plot(kind="barh")
+  plt.savefig('class_distribution.png')
+  
+  # Create a diagram on the survival ratio based on the given sex
+  plt.figure()  # Create a new figure
+  pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survive')
+  plt.savefig('survival_ratio_by_sex.png')
+  
+if __name__ == "__main__":
+  main()
+  
